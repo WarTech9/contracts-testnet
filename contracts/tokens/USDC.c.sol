87c5d7b4a76e252e2ad3917d14223b2878ba3fc3
Wartech <john.warmann@gmail.com>
2022-03-01 03:49:54 -0500

    added testnet placeholder tokens for wrapped native token and USDC

--- contracts/tokens/USDC.c.sol
+++ contracts/tokens/USDC.c.sol
@@ -1,13 +1,13 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract USDC is ERC20 {
 
     uint256 private _totalSupply = 100_000_000 ether;
 
-    constructor() ERC20("USDC.c", "USDC.c") {
+    constructor() ERC20("USDC Chedda Testnet", "USDC.c") {
         _mint(msg.sender, _totalSupply);
     }
 
---
+++ contracts/tokens/WrappedNativeToken.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/// @title Wrapped Native Token
+/// @notice This is a placeholder for wrapped native asset (WETH, WMATIC, WFTM etc.)
+/// @dev Not a real wrapped asset. Does not support converting to or from native asset.
+/// Only used in testnet for testing.
+contract WrappedNativeToken is ERC20 {
+
+    constructor(string memory name, string memory symbol, uint256 _totalSupply) 
+    ERC20(name, symbol) {
+        _mint(msg.sender, _totalSupply);
+    }
+
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
+}
